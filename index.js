c829f2622a03a99a58315a54127835bfeda1aa92
gabriel.vinhaes <gabriel.lima@citara.io>
2020-08-06 14:48:20 -0300

    Babel and rollup

--- index.js
+++
@@ -1,7 +0,0 @@
-import useJwtAuth, { JwtAuthProvider } from "./src/useJwtAuth";
-
-module.exports.JwtAuthProvider = JwtAuthProvider;
-module.exports.useJwtAuth = useJwtAuth;
-module.exports = {
-    JwtAuthProvider, useJwtAuth
-}
