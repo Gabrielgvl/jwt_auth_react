23d90bb4e72364d9a35fb72345b4567bc200beaa
gabriel.vinhaes <gabriel.lima@citara.io>
2020-09-01 11:36:46 -0300

    fixes and documentation

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 ## Jwt Auth
+
+
+#### Usage example
+
+* **Provider**
+
+* Wrap your application with the provider
+
+```javascript
+ReactDOM.render(
+      <JwtAuthProvider keyPrefix="YOUR APP KEY">
+          <App />
+      </JwtAuthProvider>,
+  document.getElementById('root'),
+);
+```
+
+* **Hooks**
+
+```javascript
+import useJwtAuth from '@gabrielgvl/jwt_auth_react';
+
+const MyComponent = () => {
+  const { handleLogin, logIn, logOut, userInfo } = useJwtAuth();
+}
+```
+
+* ```logIn(token)``` - Passes response jwt token to storage
+* ```logOut()``` - Remove token from storage
+* ```isLoggedIn``` - Check if token is in storage
+* ```userInfo``` - Retrieves decoded user info from token
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,47 @@
-// @ts-ignore
-import React, {createContext, useContext, useReducer} from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
 
 import {
-    handleLogIn,
-    handleLogOut,
-    handleSetUserInfo,
-    LOG_IN,
-    LOG_OUT,
-    SET_USER_INFO,
-    GET_TOKEN, getToken,
+  handleLogIn,
+  handleLogOut,
+  handleSetUserInfo,
+  LOG_IN,
+  LOG_OUT,
+  SET_USER_INFO,
+  GET_TOKEN, getToken,
 } from './authStore';
 
-const JwtAuthContext = createContext();
+interface InitialInterface {
+  isLoggedIn: Boolean,
+  userInfo: Object | null,
+  isLogin: Boolean,
+  cache: Object,
+  token: String,
+}
 
-const initialState = (token) => ({
-    isLoggedIn: !!localStorage.getItem(token),
-    userInfo: null,
-    isLogin: false,
-    cache: {},
-    token,
+const initialState = (token = '') => ({
+  isLoggedIn: !!localStorage.getItem(token),
+  userInfo: null,
+  isLogin: false,
+  cache: {},
+  token,
 });
 
-const jwtAuthReducer = (state, action) => {
-    switch (action.type) {
-        case SET_USER_INFO:
-            return handleSetUserInfo(state);
-        case LOG_IN:
-            return handleLogIn(state, action.payload);
-        case LOG_OUT:
-            return handleLogOut(state);
-        case GET_TOKEN:
-            return getToken(state);
-        default:
-            return state;
-    }
-};
+const JwtAuthContext = createContext<InitialInterface>(initialState());
 
-/* Export a component to provide the context to its children. This is used in our _app.js file */
+const jwtAuthReducer = (state, action) => {
+  switch (action.type) {
+    case SET_USER_INFO:
+      return handleSetUserInfo(state);
+    case LOG_IN:
+      return handleLogIn(state, action.payload);
+    case LOG_OUT:
+      return handleLogOut(state);
+    case GET_TOKEN:
+      return getToken(state);
+    default:
+      return state;
+  }
+};
 
 interface JwtAuthInterface {
     children: Node,
@@ -45,75 +50,76 @@ interface JwtAuthInterface {
 }
 
 export const JwtAuthProvider: React.FC<JwtAuthInterface> = ({ children, keyPrefix, jwtToken }) => {
-    const [state, dispatch] = useReducer(jwtAuthReducer, initialState(`${keyPrefix}:${jwtToken}`));
+  const [state, dispatch] = useReducer(jwtAuthReducer, initialState(`${keyPrefix}:${jwtToken}`));
 
-    return (
-        <JwtAuthContext.Provider value={[state, dispatch]}>
-            {children}
-        </JwtAuthContext.Provider>
-    );
+  return (
+  // @ts-ignore
+    <JwtAuthContext.Provider value={[state, dispatch]}>
+      {children}
+    </JwtAuthContext.Provider>
+  );
 };
 
 JwtAuthProvider.defaultProps = {
-    jwtToken: 'token',
-}
+  jwtToken: 'token',
+};
 
 interface UseJwtAuthReturn {
     setUserInfo: () => void,
     logIn: (token) => void,
     logOut: () => void,
-    getToken: () => void,
+    token: String | null,
     handleLogin: (token, effect?) => void,
     isLoggedIn: Boolean,
     isLogin: Boolean,
     userInfo: {},
 }
 
-const useJwtAuth: React.FC = () : UseJwtAuthReturn => {
-    const [state, dispatch] = useContext(JwtAuthContext);
+const useJwtAuth: () => UseJwtAuthReturn = () : UseJwtAuthReturn => {
+  const [state, dispatch] = useContext<any>(JwtAuthContext);
 
-    const setUserInfo = () => {
-        dispatch({
-            type: SET_USER_INFO,
-        });
-    };
+  const setUserInfo = () => {
+    dispatch({
+      type: SET_USER_INFO,
+    });
+  };
 
-    const logIn = (token) => {
-        dispatch({
-            type: LOG_IN,
-            payload: {
-                token,
-            },
-        });
-    };
+  const logIn = (token) => {
+    dispatch({
+      type: LOG_IN,
+      payload: {
+        token,
+      },
+    });
+  };
 
-    const logOut = () => {
-        dispatch({
-            type: LOG_OUT,
-        });
-    };
+  const logOut = () => {
+    dispatch({
+      type: LOG_OUT,
+    });
+  };
 
-    const handleLogin = async (token, effect = () => {}) => {
-        logIn(token);
-        effect();
-    };
+  const handleLogin = async (token, effect = () => {}) => {
+    logIn(token);
+    effect();
+  };
 
-    const getToken = () => {
-        dispatch({
-            type: GET_TOKEN,
-        })
-    }
+  // const getToken = () => {
+  //   dispatch({
+  //     type: GET_TOKEN,
+  //   });
+  // };
 
-    return {
-        setUserInfo,
-        logIn,
-        logOut,
-        handleLogin,
-        getToken,
-        isLoggedIn: state.isLoggedIn,
-        isLogin: state.isLogin,
-        userInfo: state.userInfo,
-    };
+  return {
+    setUserInfo,
+    logIn,
+    logOut,
+    handleLogin,
+    token: localStorage.getItem(state.token),
+    isLoggedIn: state.isLoggedIn,
+    isLogin: state.isLogin,
+    userInfo: state.userInfo,
+  };
 };
 
 export default useJwtAuth;
